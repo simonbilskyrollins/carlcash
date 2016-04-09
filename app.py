109891b1463e0f68a3701f1845224cbba2f8350e
Barr Iserloth <iserlothb@carleton.edu>
2016-04-09 18:22:21 -0500

    Added initial login page, limited functionality for now, need to add scraper call

--- app.py
+++ app.py
@@ -1,11 +1,18 @@
 # import the Flask class from the flask module
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for, request
 
 # create the application object
 app = Flask(__name__)
 
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+	return redirect(url_for('home'))
+    return render_template('login.html', error = error);
+
 # use decorators to link the function to a url
-@app.route('/')
+@app.route('/index.html')
 def home():
     return render_template('index.html')  # return a string
 
