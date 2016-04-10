c86ab52ad7c86a19bef10c44fd3f4c2039a8e2db
Barr Iserloth <iserlothb@carleton.edu>
2016-04-09 19:32:17 -0500

    Added scraper functionality to login! Data ahoy

--- app.py
+++ app.py
@@ -1,19 +1,34 @@
 # import the Flask class from the flask module
-from flask import Flask, render_template, redirect, url_for, request
+from flask import Flask, render_template, redirect, url_for, request, session
+import os
+import scraper
 
 # create the application object
 app = Flask(__name__)
+app.secret_key = os.urandom(24)
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     error = None
     if request.method == 'POST':
-        print request.form['username']
+	form = request.form
+	form_un = form['username']
+	form_pw = form['password']
+	mylist = [form_un, form_pw]
+	session['mylist'] = mylist
         return redirect(url_for('home'))
+
     return render_template('login.html', error = error);
 
 @app.route('/index.html')
 def home():
+    infoList = ['', '']
+    j = 0
+    for i in session.pop('mylist', []):
+	infoList[j] = i	
+	j += 1
+    JSON = scraper.main(infoList[0], infoList[1])
+    print(JSON)
     return render_template('index.html')
 
 # start the server with the 'run()' method
